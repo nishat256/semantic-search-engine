91343e6c9f33ca6ca6be269a8e9eec14e789be16
ahmadnishat25@gmail.com <ahmadnishat25@gmail.com>
2020-05-24 23:01:52 +0530

    adding flask-ngrok for running app on google collab

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
+from flask_ngrok import run_with_ngrok
 from flask import Flask, render_template, request
 from test import main_1
 app = Flask(__name__)
+run_with_ngrok(app)
 
 @app.route('/search-engine',methods=['GET','POST'])
 def index():
@@ -11,4 +13,4 @@ def index():
 	return render_template('index.html')
 
 if __name__ == '__main__':
-   app.run(host='0.0.0.0',debug = True)
+   app.run()
